0f3540438a96d02c1c4881be223339107f333dfe
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-07 15:30:50 +0700

    Using fluent api to create DonHang, DonHangChiTiet and update database

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -18,7 +18,31 @@ namespace WebApi_HienLTH.Data
         #endregion
 
         //sử dụng fluent api để tạo bảng
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DonHangEntity>(e =>
+            {
+                e.ToTable("DonHang");
+                e.HasKey(dh => dh.MaDh);
+                e.Property(dh => dh.NgatDat).HasDefaultValueSql("CURRENT_DATE");
+                e.Property(dh => dh.NguoiNhan).IsRequired().HasMaxLength(100);
+            });
 
+            modelBuilder.Entity<DonHangChiTietEntity>(e =>
+            {
+                e.ToTable("ChiTietDonHang");
+                e.HasKey(e => new { e.MaDh, e.MaHh });
 
+                e.HasOne(e => e.DonHang) // đơn hàng chi tiết ứng với 1 đơn hàng 
+                    .WithMany(e => e.DonHangChiTiets)  // Nhảy qua đơn hàng .Đơn hàng có nhiều đơn hàng chi tiết
+                    .HasForeignKey(e => e.MaDh)
+                    .HasConstraintName("FK_DonHangCT_DonHang");
+
+                e.HasOne(e => e.HangHoa)
+                    .WithMany(e => e.DonHangChiTiets)
+                    .HasForeignKey(e => e.MaHh)
+                    .HasConstraintName("FK_DonHangCT_HangHoa");
+            });
+        }
     }
 }
---
+++ Migrations/20241107081934_add-DonHang-DonHangChiTiet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi_HienLTH.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDonHangDonHangChiTiet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DonHang",
+                columns: table => new
+                {
+                    MaDh = table.Column<Guid>(type: "uuid", nullable: false),
+                    NgatDat = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    NgayGiao = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    TinhTrangDonHang = table.Column<int>(type: "integer", nullable: false),
+                    NguoiNhan = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    DiaChiGiao = table.Column<string>(type: "text", nullable: false),
+                    SoDienThoai = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonHang", x => x.MaDh);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChiTietDonHang",
+                columns: table => new
+                {
+                    MaHh = table.Column<Guid>(type: "uuid", nullable: false),
+                    MaDh = table.Column<Guid>(type: "uuid", nullable: false),
+                    SoLuong = table.Column<int>(type: "integer", nullable: false),
+                    DonGia = table.Column<double>(type: "double precision", nullable: false),
+                    GiamGia = table.Column<byte>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChiTietDonHang", x => new { x.MaDh, x.MaHh });
+                    table.ForeignKey(
+                        name: "FK_DonHangCT_DonHang",
+                        column: x => x.MaDh,
+                        principalTable: "DonHang",
+                        principalColumn: "MaDh",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DonHangCT_HangHoa",
+                        column: x => x.MaHh,
+                        principalTable: "HangHoa",
+                        principalColumn: "MaHh",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChiTietDonHang_MaHh",
+                table: "ChiTietDonHang",
+                column: "MaHh");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChiTietDonHang");
+
+            migrationBuilder.DropTable(
+                name: "DonHang");
+        }
+    }
+}
