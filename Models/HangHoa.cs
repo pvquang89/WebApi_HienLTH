90502895c905b3980c124d90b041ad16e5a56d38
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-07 13:44:31 +0700

    Add project files.

---
+++ Models/HangHoa.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi_HienLTH.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    //về mặt database
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
