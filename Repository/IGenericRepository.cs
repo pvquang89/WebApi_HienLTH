586d73780c432ac583de7bed3eb6c145e424cb41
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-08 15:01:30 +0700

    Change data type of parameter in IGenericRepository

--- Repository/IGenericRepository.cs
+++ Repository/IGenericRepository.cs
@@ -3,9 +3,9 @@
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByIdAsync(int id);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
-        Task DeleteAsync(Guid id);
+        Task DeleteAsync(int id);
     }
 }
