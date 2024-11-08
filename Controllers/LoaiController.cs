c406a51d82b0123c49a3e68fdf5591f78459b719
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-08 23:46:36 +0700

    Create LoaiController

--- Controllers/LoaiController.cs
+++ Controllers/LoaiController.cs
@@ -8,7 +8,6 @@ namespace WebApi_HienLTH.Controllers
     [ApiController]
     public class LoaiController : ControllerBase
     {
-
         private readonly IGenericRepository<LoaiModel> _loaiRepository;
 
         public LoaiController(IGenericRepository<LoaiModel> loaiRepository)
@@ -22,7 +21,46 @@ namespace WebApi_HienLTH.Controllers
             var loais = await _loaiRepository.GetAllAsync();
             return Ok(loais);
         }
-        
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var loai = await _loaiRepository.GetByIdAsync(id);
+            if (loai == null)
+                return NotFound("Loại này không tồn tại");
+            return Ok(loai);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LoaiModel>> Create(LoaiModel model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _loaiRepository.CreateAsync(model);
+            return CreatedAtAction(nameof(GetById), new { id = model.MaLoai }, model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] LoaiModel model)
+        {
+            if (id != model.MaLoai)
+                return BadRequest("ID không trùng khớp.");
+
+            await _loaiRepository.UpdateAsync(model);
+            return Ok("Cập nhật thành công");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if(!await _loaiRepository.ExistsAsync(id))
+            {
+                return NotFound("Loại này không tồn tại");
+            }
+            await _loaiRepository.DeleteAsync(id);
+            return Ok("Xoá thành công");
+        }
 
     }
 }
