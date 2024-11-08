2deb59476ba551f68d19c314a69b2fcb7383b6b5
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-08 17:02:43 +0700

    Create LoaiModel

---
+++ Models/LoaiModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_HienLTH.Models
+{
+    public class LoaiModel
+    {
+        public int MaLoai { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+    }
+}
