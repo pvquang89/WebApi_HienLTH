26ca50847f7dd64ebabfbf60c0ac068c524f70ce
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-08 16:59:17 +0700

    Create MapperProfile to map between entity and model

---
+++ Helper/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MyWebApiApp.Data;
+using WebApi_HienLTH.Models;
+
+namespace WebApi_HienLTH.Helper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            //Map 2 chiều từ LoaiEntity sang LoaiModel và ngược lại
+            CreateMap<LoaiEntity, LoaiModel>().ReverseMap();
+        }
+
+    }
+}
