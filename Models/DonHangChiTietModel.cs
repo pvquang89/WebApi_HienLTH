270bbd5ca361a859bf7e81751911f27750caaffe
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-13 14:52:59 +0700

    Create DonHangChiTietModel

---
+++ Models/DonHangChiTietModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi_HienLTH.Models
+{
+    public class DonHangChiTietModel
+    {
+        public int MaHh { get; set; }
+        public int MaDh { get; set; }
+        public int SoLuong { get; set; }
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        //Thêm tên hàng hoá
+        public string TenHangHoa { get; set; }
+    }
+}
