4ac970738055eedeaa353c91a37159e9f3c66bc7
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-18 22:44:11 +0700

    Create DonHangChiTietModel

--- Models/DonHangChiTietModel.cs
+++ Models/DonHangChiTietModel.cs
@@ -1,15 +1,19 @@
 ﻿namespace WebApi_HienLTH.Models
 {
+    //để update, create
     public class DonHangChiTietModel
     {
-        public int MaHh { get; set; }
         public int MaDh { get; set; }
+        public int MaHh { get; set; }
         public int SoLuong { get; set; }
         public double DonGia { get; set; }
         public byte GiamGia { get; set; }
+    }
 
+    //để get
+    public class DonHangChiTietModelExtend : DonHangChiTietModel
+    {
         //Thêm tên hàng hoá
         public string TenHangHoa { get; set; }
-
     }
 }
