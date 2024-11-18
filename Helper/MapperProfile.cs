3a5a41cc4177b43b9ac68a0d2c2150d05b3683a4
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-18 22:43:40 +0700

    Configure mapper for DonHangChiTiet

--- Helper/MapperProfile.cs
+++ Helper/MapperProfile.cs
@@ -14,13 +14,21 @@ namespace WebApi_HienLTH.Helper
 
             //ForMember : ánh xạ cho thuộc tính
             CreateMap<HangHoaEntity, HangHoaModel>()
-            //đoạn này : ánh xạ TenLoai từ Loai trong src vào TenLoai trong dest
-            //cách đọc tương tự CreateMap(), map từ tham số thứ 2 sang tham số thứ 1
-            .ForMember(dest => dest.TenLoai, // thuộc tính đích muốn map đến
-                        opt => opt.MapFrom(src => src.Loai.TenLoai)) //map từ src vào dest
-            .ReverseMap();
+                //đoạn này : ánh xạ TenLoai từ Loai trong src vào TenLoai trong dest
+                //cách đọc tương tự CreateMap(), map từ tham số thứ 2 sang tham số thứ 1
+                .ForMember(dest => dest.TenLoai, // thuộc tính đích muốn map đến
+                            opt => opt.MapFrom(src => src.Loai.TenLoai)) //map từ src vào dest
+                .ReverseMap();
 
             CreateMap<DonHangEntity, DonHangModel>().ReverseMap();
+
+            //cấu hình mapping cho DonHangChiTiet
+            CreateMap<DonHangChiTietEntity, DonHangChiTietModelExtend>()
+                .ForMember(dest => dest.TenHangHoa,
+                            opt => opt.MapFrom(src => src.HangHoa.TenHh));
+
+            CreateMap<DonHangChiTietEntity, DonHangChiTietModel>()
+                .ReverseMap();
 
         }
 
