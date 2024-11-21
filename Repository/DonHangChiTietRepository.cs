7aea6a943ded904969bd339363e26c3a8a58a1bd
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-21 23:46:12 +0700

    Update DonHangChiTietRepository and Controller

--- Repository/DonHangChiTietRepository.cs
+++ Repository/DonHangChiTietRepository.cs
@@ -7,10 +7,47 @@ namespace WebApi_HienLTH.Repository
 {
     public class DonHangChiTietRepository : RepositoryBase, IGenericRepository<DonHangChiTietModel>
     {
+        public static int PAGE_SIZE { get; set; } = 5;
+
         public DonHangChiTietRepository(MyDbContext context, IMapper mapper) : base(context, mapper)
         {
         }
 
+        //ở đây dù kiểu trả về là DonHangChiTietModel nhưng vẫn return DonHangChiTietModelExtend nhờ tính đa hình OOP
+        //nếu đổi kiểu trả về thành DonHangChiTietModelExtend sẽ sai vì tính kế thừa ko áp dụng cho danh sách
+        public async Task<IEnumerable<DonHangChiTietModel>> GetAllAsync(string? tenHh, int? maDh, string? sortBy, int page=1)
+        {
+            var query = _context.DonHangChiTiets.Include(e => e.HangHoa).AsQueryable();
+
+            #region Tìm theo tên, mã đơn hàng
+            if (!string.IsNullOrEmpty(tenHh))
+            {
+                query = query.Where(e => EF.Functions.Like(e.HangHoa.TenHh, $"%{tenHh}%"));
+            }
+            //HasValue : true - nếu maDh có giá trị, false nếu null
+            if (maDh.HasValue)
+            {
+                query = query.Where(e => e.MaDh == maDh.Value);
+            }
+            #endregion
+
+            #region sắp xếp
+            query = sortBy?.ToLower() switch
+            {
+                "tenhanghoa" => query.OrderBy(e => e.HangHoa.TenHh),
+                "soluong" => query.OrderBy(e => e.SoLuong),
+                "soluonggiamdan" => query.OrderByDescending(e => e.SoLuong),
+                _ => query.OrderBy(e => e.MaDh) // Mặc định sắp xếp theo mã đơn hàng
+            };
+
+            #endregion
+
+            #region paging
+            query = query.Skip((page - 1) * PAGE_SIZE).Take(PAGE_SIZE);
+            #endregion
+            var entities = await query.ToListAsync();
+            return _mapper.Map<IEnumerable<DonHangChiTietModelExtend>>(entities);
+        }
         public async Task CreateAsync(DonHangChiTietModel model)
         {
             var entity = _mapper.Map<DonHangChiTietEntity>(model);
@@ -18,14 +55,6 @@ namespace WebApi_HienLTH.Repository
             await _context.SaveChangesAsync();
         }
 
-        //ở đây dù kiểu trả về là DonHangChiTietModel nhưng vẫn return DonHangChiTietModelExtend nhờ tính đa hình OOP
-        //nếu đổi kiểu trả về thành DonHangChiTietModelExtend sẽ sai vì tính kế thừa ko áp dụng cho danh sách
-        public async Task<IEnumerable<DonHangChiTietModel>> GetAllAsync()
-        {
-            var entities = await _context.DonHangChiTiets.Include(e => e.HangHoa)
-                                .OrderBy(e => e.MaDh).ToListAsync();
-            return _mapper.Map<IEnumerable<DonHangChiTietModelExtend>>(entities);
-        }
         //nếu đổi kiểu trả về thành DonHangChiTietModelExtend vẫn đúng vì nó là 1 đối tượng đơn lẻ và kế thừa DonHangChiTietModel 
         public async Task<DonHangChiTietModelExtend> GetByIdAsync(int maDh, int maHh)
         {
@@ -71,13 +100,24 @@ namespace WebApi_HienLTH.Repository
                  AnyAsync(e => e.MaDh == maDh && e.MaHh == maHh);
         }
 
+        //1 số chức năng khác
+
+        public async Task<double> GetTotalValueByMaDh(int maDh)
+        {
+            var totalValue = await _context.DonHangChiTiets.Where(e => e.MaDh == maDh)
+                                    .SumAsync(e => e.SoLuong * e.DonGia); //ở mỗi phần tử sẽ tính như này và cuối cùng sẽ tổng lại
+            return totalValue;
+        }
+
+        //các hàm dưới đã được override 
+
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
-        public Task<bool> ExistsAsync(int id)
+        public async Task<bool> ExistsAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.DonHangChiTiets.AnyAsync(e => e.MaDh == id);
         }
 
         public Task<DonHangChiTietModel> GetByIdAsync(int id)
@@ -85,6 +125,9 @@ namespace WebApi_HienLTH.Repository
             throw new NotImplementedException();
         }
 
-
+        public Task<IEnumerable<DonHangChiTietModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
