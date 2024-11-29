e0d92cf7e4dcea1fbb9432afdf917e6a9fcdee60
pvquang89 <phamvanquang12042002@gmail.com>
2024-11-29 23:30:39 +0700

    Create model for Jwt

---
+++ Models/JwtModel/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi_HienLTH.Models.JwtModel
+{
+    //class này để đọc cấu hình từ appSetting.json vào các thuộc tính trong mã nguồn 
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+    }
+}
