cbed7bad0dacf947e8cb12b810985f84259cde48
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-04 15:56:16 +0700

    Create UserRole entity

---
+++ Data/UserRole.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_HienLTH.Data
+{
+    public class UserRole
+    {
+        //ko cần vì dùng cặp khoá tổ hợp dưới đã đủ xác định tính duy nhất mỗi dòng
+        //[Key]
+        //public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+        // Navigation properties
+        [ForeignKey("UserId")]
+        public NguoiDungEntity NguoiDung { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+    }
+}
