1a83e364613134420c54e8c1672f1c1d0b4d54f6
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-04 15:55:59 +0700

    Create Role entity

---
+++ Data/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_HienLTH.Data
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoleName { get; set; }
+    }
+}
