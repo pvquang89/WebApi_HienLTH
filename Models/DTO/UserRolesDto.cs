59d8e6c86291176a493297732cdeb9cedb5afa65
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-07 14:18:33 +0700

    Create DTO folder and create UserRolesDto

---
+++ Models/DTO/UserRolesDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_HienLTH.Models.DTO
+{
+    public class UserRolesDto
+    {
+        public int Id { get; set; }    
+        public string UserName { get; set; }
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string VaiTro { get; set; }
+    }
+}
+
