6b5484581f27c54501b71cf57294f32fd002cdae
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-07 14:17:42 +0700

    Rename folder

---
+++ Repository/AuthRepository/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApi_HienLTH.Data;
+
+namespace WebApi_HienLTH.Repository.NguoiDungRepository
+{
+    public interface IAuthRepository
+    {
+        NguoiDungEntity Authenticate (string username, string password);
+    }
+}
