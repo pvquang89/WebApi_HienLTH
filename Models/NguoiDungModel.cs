c574775cd464cb6fa4d9222b7f8a57418deb4e77
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-09 16:48:32 +0700

    Create NguoiDungModel

---
+++ Models/NguoiDungModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_HienLTH.Models
+{
+    public class NguoiDungModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Password { get; set; }
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+    }
+}
