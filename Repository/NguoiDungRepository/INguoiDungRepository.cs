e66d101c92046de22e941dc41ad867ab3263c1bc
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-09 16:49:03 +0700

    Create interface INguoiDungRepository

--- Repository/NguoiDungRepository/INguoiDungRepository.cs
+++ Repository/NguoiDungRepository/INguoiDungRepository.cs
@@ -1,9 +1,15 @@
 ﻿using WebApi_HienLTH.Data;
+using WebApi_HienLTH.Models;
+using WebApi_HienLTH.Models.DTO;
 
 namespace WebApi_HienLTH.Repository.NguoiDungRepository
 {
     public interface INguoiDungRepository
     {
-        NguoiDungEntity Authenticate (string username, string password);
+        Task<IEnumerable<UserWithRolesDto>> GetAllUserWithRolesAsync();
+        Task<IEnumerable<RoleDto>> GetAllRolesAsync();
+        Task CreateUser(NguoiDungModel model);
+        Task<IEnumerable<object>> GetUsersGroupedByRoleAsync();
+
     }
 }
