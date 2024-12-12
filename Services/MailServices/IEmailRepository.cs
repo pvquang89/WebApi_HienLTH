7d1bdb0593e6034570bcdd4d761a2405b370df9b
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-12 14:41:53 +0700

    [Feature] Add EmailServices for sending otp

---
+++ Services/MailServices/IEmailRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi_HienLTH.Services.MailServices
+{
+    public interface IEmailRepository
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
