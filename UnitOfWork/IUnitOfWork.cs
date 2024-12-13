57e81b579214a19ed0aca0d653d5353bec98f038
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-13 22:06:42 +0700

    [Feature] Add IUnitOfWork interface

---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using WebApi_HienLTH.Models;
+using WebApi_HienLTH.Repository;
+using WebApi_HienLTH.Repository.NguoiDungRepository;
+using WebApi_HienLTH.Repository.Repository;
+
+namespace WebApi_HienLTH.UnitOfWork
+{
+    //IDisposable : giải phóng tài nguyên khi ko quản lý
+    public interface IUnitOfWork : IDisposable
+    {
+        //các repository implement IGenericRepository
+        IGenericRepository<DonHangModel> DonHangRepository { get; }
+        IGenericRepository<HangHoaModel> HangHoaRepository { get; }
+        IGenericRepository<LoaiModel> LoaiRepository { get; }
+
+        //các repository cụ thể 
+        DonHangChiTietRepository DonHangChiTietRepository { get; } 
+        IAuthRepository AuthRepository { get; }
+        INguoiDungRepository NguoiDungRepository { get; }
+        //
+        Task SaveAsync();
+    }
+}
